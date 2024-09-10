d634523d90de03cd6852ff56b5d0e6788055833d
Vanderson Batista <vandersonb@gmail.com>
2024-09-10 12:24:42 -0300

    Construindo o layout da page Desktop

--- lib/pages/desktop_body.dart
+++ lib/pages/desktop_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyDesktopBody extends StatelessWidget {
   const MyDesktopBody({super.key});
@@ -6,7 +7,31 @@ class MyDesktopBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepOrangeAccent[300],
+      backgroundColor: Colors.deepPurple[300],
+      appBar: AppBar(
+        title: const Text('Home Desktop'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                // Cria a lista com construtor
+                itemCount: 6, // Define a quantidade de itens na lista
+                itemBuilder: (context, index) {
+                  // Construtor
+                  return Padding(
+                    // Define um espaço em volta de cada item
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      // Cria cada container
+                      color: Colors.deepPurple[400],
+                      height: 280,
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/mobile_body.dart
+++ lib/pages/mobile_body.dart
@@ -7,6 +7,49 @@ class MyMobileBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[300],
+      appBar: AppBar(
+        title: const Text('Home Mobile'),
+      ),
+      body: Column(
+        // definindo uma coluna
+        children: [
+          // youtube video
+
+          // Que tem filhos
+          Padding(
+            // Envolvendo o Container com um Padding de 8.0 em totos os lados.
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+              // Vamos agrupar o container
+              aspectRatio: 16 / 9, // Com isso podemos remover a altura.
+              child: Container(
+                // Definindo um container
+                // height: 250, // com altura de 250.
+                color: Colors.deepPurple[400], // E cor de fundo deepPurple[400]
+              ),
+            ),
+          ),
+          // seção de comentários e vídeos recomendados
+          // criar uma visualização de lista.
+          Expanded(
+            child: ListView.builder(
+                // Cria a lista com construtor
+                itemCount: 8, // Define a quantidade de itens na lista
+                itemBuilder: (context, index) {
+                  // Construtor
+                  return Padding(
+                    // Define um espaço em volta de cada item
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      // Cria cada container
+                      color: Colors.deepPurple[400],
+                      height: 120,
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
