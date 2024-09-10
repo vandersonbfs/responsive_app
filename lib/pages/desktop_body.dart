32c3edb02be482e437512da0a35eee4814a9024e
Vanderson Batista <vandersonb@gmail.com>
2024-09-10 12:41:09 -0300

    Desktop page

--- lib/pages/desktop_body.dart
+++ lib/pages/desktop_body.dart
@@ -11,25 +11,61 @@ class MyDesktopBody extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Desktop'),
       ),
-      body: Column(
+      body: Row(
         children: [
+          // Primeira coluna
           Expanded(
-            child: ListView.builder(
-                // Cria a lista com construtor
-                itemCount: 6, // Define a quantidade de itens na lista
-                itemBuilder: (context, index) {
-                  // Construtor
-                  return Padding(
-                    // Define um espaço em volta de cada item
-                    padding: const EdgeInsets.all(8.0),
+            // Definido para possa monstrar a segunda coluna.
+            child: Column(
+              // definindo uma coluna
+              children: [
+                // youtube video
+
+                // Que tem filhos
+                Padding(
+                  // Envolvendo o Container com um Padding de 8.0 em totos os lados.
+                  padding: const EdgeInsets.all(8.0),
+                  child: AspectRatio(
+                    // Vamos agrupar o container
+                    aspectRatio: 16 / 9, // Com isso podemos remover a altura.
                     child: Container(
-                      // Cria cada container
-                      color: Colors.deepPurple[400],
-                      height: 280,
+                      // Definindo um container
+                      // height: 250, // com altura de 250.
+                      color: Colors
+                          .deepPurple[400], // E cor de fundo deepPurple[400]
                     ),
-                  );
-                }),
+                  ),
+                ),
+                // seção de comentários e vídeos recomendados
+                // criar uma visualização de lista.
+                Expanded(
+                  child: ListView.builder(
+                      // Cria a lista com construtor
+                      itemCount: 8, // Define a quantidade de itens na lista
+                      itemBuilder: (context, index) {
+                        // Construtor
+                        return Padding(
+                          // Define um espaço em volta de cada item
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            // Cria cada container
+                            color: Colors.deepPurple[400],
+                            height: 120,
+                          ),
+                        );
+                      }),
+                ),
+              ],
+            ),
           ),
+          // Segunda coluna
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 200,
+              color: Colors.deepPurple[400],
+            ),
+          )
         ],
       ),
     );
